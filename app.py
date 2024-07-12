9d6854aa080061e19895fb57f0624e04b84e0d83
zateen <zatinfarhah16@gmail.com>
2024-07-12 11:42:07 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Welcome to Streamlit Development Class!')
+st.write('We will learning how to create Web Apps using Python programming with Streamlit Library')
+st.warning('Stay Tune!')
+st.success('Almost there!')
+st.header('A...')
+st.subheader('B...')
+st.balloons()
+st.success('Done!')
